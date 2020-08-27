8449eb017c1dcc82b38372160035c4e793b8be23
Shubham Prasad Singh <sprasad3101@gmail.com>
2020-08-27 20:21:46 +0530

    Added custom template filter for datetime.

--- pwned_credentials.py
+++ pwned_credentials.py
@@ -1,3 +1,9 @@
 from app import create_app
+import datetime
 
 app = create_app()
+
+
+@app.template_filter("datetime")
+def format_datetime(value, from_format, to_format):
+    return datetime.datetime.strptime(value, from_format).strftime(to_format)
